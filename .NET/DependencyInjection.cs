18aaa0b8ac31d6c144dd1304fb0c418efc33a73c
Porfirio Alvarez <127803056+AimBoosh@users.noreply.github.com>
2023-07-19 11:56:11 -0700

    Updated DependencyInjection.cs
    Removed unnecessary code.

--- .NET/DependencyInjection.cs
+++ .NET/DependencyInjection.cs
@@ -3,19 +3,19 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Sabio.Data;
-using Sabio.Models.Domain.Venues;
-using Sabio.Services;
-using Sabio.Services.Interfaces;
-using Sabio.Services.Interfaces.Survey;
-using Sabio.Services.Surveys;
-using Sabio.Web.Api.StartUp.DependencyInjection;
-using Sabio.Web.Core.Services;
+using  Data;
+using  Models.Domain.Venues;
+using  Services;
+using  Services.Interfaces;
+using  Services.Interfaces.Survey;
+using  Services.Surveys;
+using  Web.Api.StartUp.DependencyInjection;
+using  Web.Core.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Sabio.Web.StartUp
+namespace  Web.StartUp
 {
     public class DependencyInjection
     {
@@ -43,114 +43,18 @@ namespace Sabio.Web.StartUp
 
             services.AddSingleton<IAuthenticationService<int>, WebAuthenticationService>();
 
-            services.AddSingleton<Sabio.Data.Providers.IDataProvider, SqlDataProvider>(delegate (IServiceProvider provider)
+            services.AddSingleton< Data.Providers.IDataProvider, SqlDataProvider>(delegate (IServiceProvider provider)
             {
                 return new SqlDataProvider(connString);
             }
             );
 
-            services.AddSingleton<IAppointmentService, AppointmentService>();
-
-            services.AddSingleton<IBaseUserMapper, UserService>();
-
-            services.AddSingleton<IBlogService, BlogService>();
-
-            services.AddSingleton<ICandidatePreferenceServices, CandidatePreferenceServices>();
-
-            services.AddSingleton<ICharitableServices, CharitableServices>();
-
-            services.AddSingleton<ICommentService, CommentsService>();
-
-            services.AddSingleton<ICompensationService, CompensationService>();
-
-            services.AddSingleton<IDegreeService, DegreeService>();
-
-            services.AddSingleton<IEducationRequirementsService, EducationRequirementsService>();
-
-            services.AddSingleton<IEmailSenderService, EmailSenderService>();
-
-            services.AddSingleton<IExperienceService,  ExperienceService>();
-
-            services.AddSingleton<IFAQsService, FAQsService>();
-
-            services.AddSingleton<IGoalService,  GoalService>();
-
-            services.AddSingleton<IGoogleAnalyticsReportService, GoogleAnalyticsReportService>();
-
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-            services.AddSingleton<IIdentityProvider<int>, WebAuthenticationService>();
-
-            services.AddSingleton<IInviteService, InviteService>();
-
-            services.AddSingleton<IJobService, JobService>();
-
-            services.AddSingleton<IJobSkillsService, JobSkillService>();
 
             services.AddSingleton<ILicenseServices, LicenseServices>();
 
-            services.AddSingleton<ILocationService, LocationService>();
-
             services.AddSingleton<ILookUpService, LookUpService>();
 
-            services.AddSingleton<IMessageService, MessageService>(); 
-
-            services.AddSingleton<INewsletterService, NewsletterService>();
-
-            services.AddSingleton<INewsletterContentService, NewsletterContentService>();
-
-            services.AddSingleton<INewsletterSubscriptionService, NewsletterSubscriptionService>();
-            services.AddSingleton<INewsletterTemplateMapperService, NewsletterTemplateService>();
-            services.AddSingleton<INewsletterTemplateService, NewsletterTemplateService>();
-
-            services.AddSingleton<IOrganizationMemberService , OrganizationMemberService>();
-            services.AddSingleton<IOrganizationService, OrganizationService>(); 
-
             services.AddSingleton<IPersonalValuesService, PersonalValuesService>();
-
-            services.AddSingleton<IPodcastServices, PodcastServices>();
-
-            services.AddSingleton<IRatingService, RatingService>();
-
-            services.AddSingleton<ISchoolMapperService, SchoolService>();
-            
-            services.AddSingleton<ISchoolService, SchoolService>();
-
-            services.AddSingleton<IShareStoryService, ShareStoryService>();
-
-            services.AddSingleton<ISiteReferencesService, SiteReferencesService>();
-
-            services.AddSingleton<ISkillService, SkillService>();
-
-            services.AddSingleton<IStripeService, StripeService>();
-
-            services.AddSingleton<ISurveyAnswerService, SurveyAnswerService>();
-
-            services.AddSingleton<ISurveyInstanceService, SurveyInstanceService>();
-
-            services.AddSingleton<ISurveyQuestionAnswerOptions, SurveyQuestionAnswerOptionsService>();
-
-            services.AddSingleton<ISurveyQuestionsService, SurveyQuestionsService>();
-
-            services.AddSingleton<ISurveyService, SurveyService>();
-
-            services.AddSingleton<ITeamService, TeamService>();
-
-            services.AddSingleton<IUserEducationService, UserEducationService>(); 
-
-            services.AddSingleton<IUserService, UserService>();
-            
-            services.AddSingleton<IExternalLinkService, ExternalLinksService>();
-
-            services.AddSingleton<IUserSkillService, UserSkillService>();
-
-            services.AddSingleton<IVenuesService, VenuesService>();
-
-            services.AddSingleton<IVideochatService, VideochatService>();
-
-            services.AddSingleton<IVideochatStatisticsService, VideochatStatisticsService>();
-
-            services.AddSingleton<IWorkHistoryService, WorkHistoryService>();
 
             services.AddSingleton<IForumsService, ForumsService>();
 
