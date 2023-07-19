9c3c12fa9b5b6d0f5d416430b1c696262f1948c3
Porfirio Alvarez <127803056+AimBoosh@users.noreply.github.com>
2023-07-19 11:59:37 -0700

    Updated RelatedPersonalValues.cs

--- .NET/RelatedPersonalValues.cs
+++ .NET/RelatedPersonalValues.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Domain.PersonalValues
+namespace  Models.Domain.PersonalValues
 {
     public class RelatedPersonalValues 
     {
