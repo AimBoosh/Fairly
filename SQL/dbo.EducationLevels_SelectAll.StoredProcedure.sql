5229822e95fb212b4c8925155f3db458e26e2238
Porfirio Alvarez <127803056+AimBoosh@users.noreply.github.com>
2023-07-27 14:53:17 -0700

    Update dbo.EducationLevels_SelectAll.StoredProcedure.sql

--- SQL/dbo.EducationLevels_SelectAll.StoredProcedure.sql
+++ SQL/dbo.EducationLevels_SelectAll.StoredProcedure.sql
@@ -1,12 +1,3 @@
-﻿USE [Fairly]
-GO
-/****** Object:  StoredProcedure [dbo].[EducationLevels_SelectAll]    Script Date: 7/12/2023 1:53:28 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-
 	CREATE PROC [dbo].[EducationLevels_SelectAll]
 
 	as
@@ -17,10 +8,10 @@ GO
 */
 	BEGIN
 
-			SELECT [Id]
-				  ,[Name]
-			  FROM [dbo].[EducationLevels]
-			  ORDER BY [Name] ASC
+		SELECT [Id]
+			,[Name]
+		
+		FROM [dbo].[EducationLevels]
+		ORDER BY [Name] ASC
 
 	END
-GO
