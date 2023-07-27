8da61a4d1ac341c42951e68eca07f155ec755630
Porfirio Alvarez <127803056+AimBoosh@users.noreply.github.com>
2023-07-27 14:19:30 -0700

    RelatedPersonalValues.cs

--- .NET/RelatedPersonalValues.cs
+++ .NET/RelatedPersonalValues.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace  Models.Domain.PersonalValues
-{
-    public class RelatedPersonalValues 
+   public class RelatedPersonalValues 
     {
       
         public LookUp PersonalValueA { get; set; }
@@ -15,4 +6,3 @@ namespace  Models.Domain.PersonalValues
    
      
     }
-}
