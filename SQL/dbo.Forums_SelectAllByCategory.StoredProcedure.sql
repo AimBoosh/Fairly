bfebcbbf36c93fa0d4bb560307df9aa274e8e725
Porfirio Alvarez <127803056+AimBoosh@users.noreply.github.com>
2023-07-27 14:54:31 -0700

    Update dbo.Forums_SelectAllByCategory.StoredProcedure.sql

--- SQL/dbo.Forums_SelectAllByCategory.StoredProcedure.sql
+++ SQL/dbo.Forums_SelectAllByCategory.StoredProcedure.sql
@@ -1,15 +1,6 @@
-﻿USE [Fairly]
-GO
-/****** Object:  StoredProcedure [dbo].[Forums_SelectAllByCategory]    Script Date: 6/23/2023 6:43:17 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-
-   CREATE PROC [dbo].[Forums_SelectAllByCategory]
+﻿   	CREATE PROC [dbo].[Forums_SelectAllByCategory]
 										
-   AS
+   	AS
 
 /*------------ TEST CODE ------------
 
@@ -17,18 +8,15 @@ EXECUTE [dbo].[Forums_SelectAllByCategory]
 															
 */
 
-   BEGIN
+   	BEGIN
 
-   SELECT	FC.Id,
+   	SELECT	FC.Id,
 			FC.[Name] AS ForumCategoryName,
 			List = (
-					SELECT F.[Name] FROM dbo.Forums AS F
-					WHERE F.ForumCategoryId = FC.Id
-					FOR JSON AUTO
-					)
-    FROM dbo.ForumCategories AS FC
+				SELECT F.[Name] FROM dbo.Forums AS F
+				WHERE F.ForumCategoryId = FC.Id
+				FOR JSON AUTO
+				)
+    	FROM dbo.ForumCategories AS FC
     
 	END
-   
-
-GO
