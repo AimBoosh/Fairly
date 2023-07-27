e1e26d41a7685a0435f1efc00b596e035006690e
Porfirio Alvarez <127803056+AimBoosh@users.noreply.github.com>
2023-07-27 14:11:47 -0700

    DependencyInjection.cs

--- .NET/DependencyInjection.cs
+++ .NET/DependencyInjection.cs
@@ -1,45 +1,17 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using  Data;
-using  Models.Domain.Venues;
-using  Services;
-using  Services.Interfaces;
-using  Services.Interfaces.Survey;
-using  Services.Surveys;
-using  Web.Api.StartUp.DependencyInjection;
-using  Web.Core.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace  Web.StartUp
-{
     public class DependencyInjection
     {
         public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
         {
             if (configuration is IConfigurationRoot)
             {
-                services.AddSingleton<IConfigurationRoot>(configuration as IConfigurationRoot);   // IConfigurationRoot
+                services.AddSingleton<IConfigurationRoot>(configuration as IConfigurationRoot);  
             }
 
-            services.AddSingleton<IConfiguration>(configuration);   // IConfiguration explicitly
+            services.AddSingleton<IConfiguration>(configuration); 
 
             services.AddSingleton<IFileService, FileService>();
 
             string connString = configuration.GetConnectionString("Default");
-            // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-2.2
-            // The are a number of differe Add* methods you can use. Please verify which one you
-            // should be using services.AddScoped<IMyDependency, MyDependency>();
-
-            // services.AddTransient<IOperationTransient, Operation>();
-
-            // services.AddScoped<IOperationScoped, Operation>();
-
-            // services.AddSingleton<IOperationSingleton, Operation>();
 
             services.AddSingleton<IAuthenticationService<int>, WebAuthenticationService>();
 
@@ -48,7 +20,6 @@ namespace  Web.StartUp
                 return new SqlDataProvider(connString);
             }
             );
-
 
             services.AddSingleton<ILicenseServices, LicenseServices>();
 
@@ -61,9 +32,7 @@ namespace  Web.StartUp
             GetAllEntities().ForEach(tt =>
             {
                 IConfigureDependencyInjection idi = Activator.CreateInstance(tt) as IConfigureDependencyInjection;
-
-                //This will not error by way of being null. BUT if the code within the method does
-                // then we would rather have the error loadly on startup then worry about debuging the issues as it runs
+                
                 idi.ConfigureServices(services, configuration);
             });
         }
@@ -79,4 +48,4 @@ namespace  Web.StartUp
         {
         }
     }
-}
+
