4af46196131e11f03d6aade4ed5bfab5597d03ef
Porfirio Alvarez <127803056+AimBoosh@users.noreply.github.com>
2023-07-27 14:17:47 -0700

    LicenseUpdateRequest.cs

--- .NET/LicenseUpdateRequest.cs
+++ .NET/LicenseUpdateRequest.cs
@@ -1,12 +1,5 @@
-using  Models;
-using System;
-using System.ComponentModel.DataAnnotations;
-
-namespace  Models.Requests.Licenses
-{
     public class LicenseUpdateRequest : LicenseAddRequest, IModelIdentifier
     {
         public int Id { get; set; }
         public DateTime? DateVerified { get; set; }
     }
-}
